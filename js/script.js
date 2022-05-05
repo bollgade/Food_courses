dfb36385b03df5c5bb23cbbd4dac0551c88f1178
bollgade <bollgade@yandex.ru>
2022-05-05 13:43:00 +0300

    Повторяя за уроком, изменил способ связи с сервером

--- js/script.js
+++ js/script.js
@@ -290,14 +290,8 @@ window.addEventListener('DOMContentLoaded', () => {
             display: block;
             margin: 0 auto;
          `;
-         // form.append(statusMessage);
          form.insertAdjacentElement('afterend', statusMessage);
-         // showThanksModal(message.loading);
 
-         const request = new XMLHttpRequest();
-         request.open('POST', 'server.php');
-
-         // request.setRequestHeader('Content-type', 'application/json');
          const formData = new FormData(form);
 
          const object = {};
@@ -305,22 +299,25 @@ window.addEventListener('DOMContentLoaded', () => {
             object[key] = value;
          });
 
-         const json = JSON.stringify(object);
-
-         request.send(json);
-
-         request.addEventListener('load', () => {
-            if (request.status === 200) {
-
-               console.log(request.response);
+         fetch('server.php', {
+            method: 'POST',
+            headers: {
+               'Content-type': 'application/json',
+            },
+            body: JSON.stringify(object),
+         })
+            .then(data => data.text())
+            .then(data => {
+               console.log(data);
                showThanksModal(message.success);
-
                form.reset();
                statusMessage.remove();
-            } else {
+            }).catch(() => {
                showThanksModal(message.failure);
-            };
-         })
+            }).finally(() => {
+               form.reset();
+            });
+
       });
    };
 
@@ -347,4 +344,5 @@ window.addEventListener('DOMContentLoaded', () => {
          closeModal();
       }, 4000);
    };
+
 });
